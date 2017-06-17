ca154c41c32620534af52e7dce750f4abbe09a76
gauraviitp <cgauraviitp@gmail.com>
2017-06-17 17:01:51 +0100

    Update CA_IOUserDefinedTypes_SecFourThreeThree.cpp

--- CA_IOUserDefinedTypes_SecFourThreeThree.cpp
+++ CA_IOUserDefinedTypes_SecFourThreeThree.cpp
@@ -5,11 +5,34 @@ struct Entry{
   string name;
   int number;
 };
-ostream& operator<<(ostream& os, const Entry& e){
+// operator "put to"
+ostream& operator<<(ostream& os, const Entry& e) {
     return os << "{\"" + e.name << "\", " << e.number << "}";
+}
+// operator "read from"
+istream& operator>>(istream& is, Entry& e) {
+    char c, c2;
+    if(is >> c && c == '{' && is >> c2 && c2 == '"') {
+        string name;
+        while(is.get(c) && c!='"'){
+            name += c;   
+        }
+        if(is >> c && c == ','){
+            int number = 0;
+            if(is >> number >> c && c=='}'){
+                e = {name, number};
+                return is;
+            }
+        }
+    }
+    is.setstate(ios_base::failbit);
+    return is;
 }
 int main() {
 	Entry e{"Gaurav", 1};
 	cout << e << '\n';
+	cout << "Enter name and number" << '\n';
+	cin >> e;
+	cout << e;
 	return 0;
 }
