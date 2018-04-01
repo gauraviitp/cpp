b831c3b25fffafd878a1bdb40a8110c45eb407a1
Gaurav Choudhary <cgauraviitp@gmail.com>
2018-04-02 00:40:58 +0100

    Update CheckPermutationSorting.cpp

--- CheckPermutationSorting.cpp
+++ CheckPermutationSorting.cpp
@@ -12,12 +12,7 @@ bool checkPermutation(string& a, string& b){
 	if(a.length() != b.length()) return false;
 	sort(a.begin(), a.end());
 	sort(b.begin(), b.end());
-	for(int i = 0; i < a.length(); ++i){
-		if(a[i] != b[i]){
-			return false;
-		}
-	}
-	return true;
+	return (a.compare(b) == 0);
 }
 
 int main(){
