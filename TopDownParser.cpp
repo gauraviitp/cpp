62f2669711b471935eac97f6bbd104419a55388f
Gaurav Choudhary <cgauraviitp@gmail.com>
2018-06-22 19:00:34 +0100

    Create TopDownParser.cpp

---
+++ TopDownParser.cpp
@@ -0,0 +1,90 @@
+// Top down parser
+
+// (+ a b)
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+enum TokenType {
+	operation,
+	leftParanthesis,
+	rightParanthesis,
+	number
+};
+
+class Token{
+public:
+	TokenType type;
+	string value;
+	Token(TokenType ptype, string pvalue):
+		  type(ptype), value(pvalue){}
+};
+
+typedef vector<Token *> Tokens;
+
+int expression(Tokens &tokens, int &pos){
+	int res = 0;
+	// First production rule
+	if(tokens[pos]->type == number){
+		res = stoi(tokens[pos]->value);
+		++pos;
+	}
+	// Second production rule
+    else if (tokens[pos]->type == leftParanthesis){
+		++pos; // op token
+		if(tokens[pos]->type == operation){
+			string op = tokens[pos]->value;
+			++pos; // first expression
+			int first = expression(tokens, pos);
+			int second = expression(tokens, pos);
+			if(op == "+"){
+				res = first + second;
+			}
+			else if (op == "-"){
+				res = first - second;
+			}
+			else if (op == "*"){
+				res = first * second;
+			}
+			else if (op == "/"){
+				res = first / second;
+			}
+			if(tokens[pos]->type == rightParanthesis){
+				++pos;
+			}
+			else {
+				cout << "Error while parsing. Expected: \")\", Actual: \"" << tokens[pos]->value << "\"" << endl;
+				throw "Error";
+			}
+		}
+		else {
+			cout << "Error while parsing. Expected: \"operation\", Actual: \"" << tokens[pos]->value << "\"" << endl;
+			throw "Error";
+		}
+	}
+	else {
+		cout << "Error while parsing. Expected: \"expression\", Actual: \"" << tokens[pos]->value << "\"" << endl;
+		throw "Error";
+	}
+	return res;
+}
+
+int parse(Tokens &tokens){
+	int pos = 0;
+	return expression(tokens, pos);
+}
+
+int main(){
+	Tokens tokens = {
+			new Token(leftParanthesis, "("),
+			new Token(operation, "+"),
+			new Token(number, "1"),
+			new Token(number, "2"),
+			new Token(rightParanthesis, ")")
+		};
+	int result = parse(tokens);
+	cout << result << endl;
+}
